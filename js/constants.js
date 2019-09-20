4db36db8e9b0ed0ea53edec08d72607f843077cd
OriolInvernonLlaneza <93silf@gmail.com>
2019-09-20 13:56:12 +0200

    refactor, style

--- js/constants.js
+++ js/constants.js
@@ -1,2 +1,18 @@
-const zoom = 5.3;
-const oviedo = [43.3602900, -5.8447600]
+const contador = 1;
+const selected_marker = null;
+const WMS_URL = 'http://ide.gijon.es:8080/geoserver/wms?';
+let WMS_Layers = '';//'Gijon:Rios,Gijon:LU_Zona_Verde,Gijon:Rutas_Verdes,Gijon:Zona_Verde,Gijon:Playas,Gijon:Instalaciones_Deportivas,Gijon:Colegios_Electorales';
+let map;
+const markersArray = [];
+const TileWMS = function (coord, zoom) {
+    const proj = map.getProjection();
+    const zfactor = Math.pow(2, zoom);
+    const top = proj.fromPointToLatLng(new google.maps.Point(coord.x * 256 / zfactor, coord.y * 256 / zfactor));
+    const bot = proj.fromPointToLatLng(new google.maps.Point((coord.x + 1) * 256 / zfactor, (coord.y + 1) * 256 / zfactor));
+    const bbox = top.lng() + "," + bot.lat() + "," + bot.lng() + "," + top.lat();
+
+    let myURL = WMS_URL + "SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&SRS=EPSG%3A4326&WIDTH=256&HEIGHT=256&FORMAT=image/png&TRANSPARENT=TRUE";
+    myURL += "&LAYERS=" + WMS_Layers;
+    myURL += "&BBOX=" + bbox;
+    return myURL;
+}
--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
-let map;
-
-function initMap() {
-    options = {
-        center: new google.maps.LatLng(oviedo[0], oviedo[1]),
-        zoom: zoom,
-        mapTypeId: google.maps.MapTypeId.SATELLITE,
-        disableDefaultUI: true,
+function initialize() {
+    const gijon = new google.maps.LatLng(43.540935, -5.673168);
+    const misOpciones = {
+        zoom: 13,
+        center: gijon,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    map = new google.maps.Map(document.getElementById("map_frame"), options);
+    map = new google.maps.Map(document.getElementById("map_canvas"), misOpciones);
+
+    const overlayOptions =
+    {
+        getTileUrl: TileWMS,
+        tileSize: new google.maps.Size(256, 256)
+    };
+    const overlayWMS = new google.maps.ImageMapType(overlayOptions);
+    map.overlayMapTypes.push(overlayWMS);
+}
+
+function check(cb) {
+    let checked = cb.checked;
+    if (checked) {
+        if (WMS_Layers === "") {
+            WMS_Layers = WMS_Layers.concat(cb.value);
+        } else {
+            WMS_Layers = WMS_Layers.concat("," + cb.value);
+        }
+        console.log(cb.value);
+    } else {
+        WMS_Layers = WMS_Layers.replace(`,${cb.value}`, '');
+        WMS_Layers = WMS_Layers.replace(`${cb.value}`, '');
+        if (WMS_Layers.charAt(0) === ",") {
+            WMS_Layers = WMS_Layers.slice(1);
+        }
+    }
+    initialize();
 }
